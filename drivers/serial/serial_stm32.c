476c3617c77d4733aa8557c6b7be3878b7d65a14
Kamil Lulko <rev13@wp.pl>
2014-09-11 21:52:28 +0200

    Add stm32 gpio driver, update stm32 serial driver

--- drivers/serial/serial_stm32.c
+++ drivers/serial/serial_stm32.c
@@ -9,29 +9,33 @@
 #include <command.h>
 #include <serial.h>
 #include <linux/compiler.h>
-
-#define STM32_RCC_BASE		0x40023800 /* AHB1 */
-#define STM32_GPIOA_BASE	0x40020000 /* AHB1 */
-
-#define CONFIG_STM32_USART1
+#include <asm/arch/stm32.h>
+#include <asm/arch/stm32_gpio.h>
 
 #define STM32_USART1_BASE	0x40011000 /* APB2 */
 #define STM32_USART2_BASE	0x40004400 /* APB1 */
 
 #if defined(CONFIG_STM32_USART1)
 #define USART_BASE	STM32_USART1_BASE
+#define USART_SR_FLAG_RXNE	0x20
+#define USART_SR_FLAG_TXE	0x80
+#define RCC_USART_ENABLE	0x10
+static const struct stm32_gpio_dsc usart_gpio[] = {
+		{STM32_GPIO_PORT_A, STM32_GPIO_PIN_9},
+		{STM32_GPIO_PORT_A, STM32_GPIO_PIN_10},
+};
 #elif defined(CONFIG_STM32_USART2)
 #define USART_BASE	STM32_USART2_BASE
 #endif
 
 struct stm32_serial {
-	volatile uint32_t USART_SR;
-	volatile uint32_t USART_DR;
-	volatile uint32_t USART_BRR;
-	volatile uint32_t USART_CR1;
-	volatile uint32_t USART_CR2;
-	volatile uint32_t USART_CR3;
-	volatile uint32_t USART_GTPR;
+	uint32_t USART_SR;
+	uint32_t USART_DR;
+	uint32_t USART_BRR;
+	uint32_t USART_CR1;
+	uint32_t USART_CR2;
+	uint32_t USART_CR3;
+	uint32_t USART_GTPR;
 };
 
 static void stm32_serial_setbrg(void)
@@ -41,16 +45,20 @@ static void stm32_serial_setbrg(void)
 
 static int stm32_serial_init(void)
 {
+	int i, rv;
 	volatile struct stm32_serial* base = (struct stm32_serial *)USART_BASE;
+	volatile struct stm32_rcc_regs* rcc = STM32_RCC_BASE;
 
 	/* Enable clocks to peripherals (GPIO, USART) */
-	*(uint32_t*)(STM32_RCC_BASE + 0x44) |= 0x10; /* USART2 enable */
-	*(uint32_t*)(STM32_RCC_BASE + 0x30) |= 0x00000001; /* GPIOA enable */
+	rcc->apb2enr |= RCC_USART_ENABLE; /* USART2 enable */
 
 	/* Enable and mux GPIOs */
-	*(uint32_t*)(STM32_GPIOA_BASE + 0x00) |= 0x280000; /* alternate function mode GPIOA pin 9,10 */
-	*(uint32_t*)(STM32_GPIOA_BASE + 0x08) |= 0x280000; /* 50MHz speed */
-	*(uint32_t*)(STM32_GPIOA_BASE + 0x24) |= 0x770; /* AF7 on pin 9,10 */
+	for (i = 0; i < ARRAY_SIZE(usart_gpio); i++) {
+		rv = stm32_gpio_config(&usart_gpio[i], STM32_GPIO_ROLE_USART1);
+		if(rv) {
+			return rv;
+		}
+	}
 
 	/* USART configuration */
 	/* for 115.200k program 22.8125 if fpclk = 42MHz */
@@ -63,14 +71,14 @@ static int stm32_serial_init(void)
 static int stm32_serial_getc(void)
 {
 	volatile struct stm32_serial* base = (struct stm32_serial *)USART_BASE;
-	while((base->USART_SR & 0x20) == 0);
+	while((base->USART_SR & USART_SR_FLAG_RXNE) == 0);
 	return base->USART_DR;
 }
 
 static void stm32_serial_putc(const char c)
 {
 	volatile struct stm32_serial* base = (struct stm32_serial *)USART_BASE;
-	while((base->USART_SR & 0x80) == 0);
+	while((base->USART_SR & USART_SR_FLAG_TXE) == 0);
 	base->USART_DR = c;
 }
 
